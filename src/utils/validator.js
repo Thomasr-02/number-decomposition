2e770faebce80dc51abcb401d2ed3bd486400a74
Thomas <thomasaraujo@mat.ci.ufpb.br>
2022-02-22 16:57:21 -0300

    fix: adjust validator

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -2,7 +2,6 @@ const validator = (input) => {
   if (!input.length) throw new Error('Entrada não pode ser vazia');
   if (isNaN(input)) throw new Error('Entrada não é um número');
   if (input <= 0) throw new Error('Entrada não pode ser negativa');
-  if (!Number.isInteger(input)) throw new Error('Entrada não é um número inteiro');
   
   return parseInt(input);
 }
